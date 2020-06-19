12c6e8f69f18234fc4d94718160c41ccb908bd23
2TallTyler <55058389+2TallTyler@users.noreply.github.com>
2020-06-19 19:50:37 -0400

    Add states

--- MAKEFILE.bat
+++ MAKEFILE.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
 
 ECHO Batch: Running NMLC compiler
-%~dp0\nmlc\nmlc -t src\custom_tags.txt -l src\lang --grf new_england_town_names.grf src\new_england_town_names.nml
+%~dp0\nmlc\nmlc -l massachusetts\lang --grf massachusetts_town_names.grf massachusetts\massachusetts_town_names.nml
+%~dp0\nmlc\nmlc -l colorado\lang --grf colorado_town_names.grf colorado\colorado_town_names.nml
+%~dp0\nmlc\nmlc -l north_carolina\lang --grf north_carolina_town_names.grf north_carolina\north_carolina_town_names.nml
+%~dp0\nmlc\nmlc -l pennsylvania\lang --grf pennsylvania_town_names.grf pennsylvania\pennsylvania_town_names.nml
 
 ECHO Batch: Cleaning up
 RMDIR %~dp0\.nmlcache
@@ -12,6 +15,10 @@ if /I "%c%" EQU "TT" (goto :copy
 
 :copy
 ECHO Copying to NewGRF directory
-copy /y /v %~dp0\new_england_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\massachusetts_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\colorado_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\north_carolina_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\pennsylvania_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+
 pause
 exit
