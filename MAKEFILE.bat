3a09f31cfbcce0fd2cd878a3a99d4fe2b0d8cd74
Tyler Trahan <tyler@tylertrahan.com>
2021-05-07 08:07:20 -0400

    Add: Cuba Town Names

--- MAKEFILE.bat
+++ MAKEFILE.bat
@@ -5,6 +5,7 @@ ECHO Batch: Running NMLC compiler
 %~dp0\nmlc\nmlc -l colorado\lang --grf colorado_town_names.grf colorado\colorado_town_names.nml
 %~dp0\nmlc\nmlc -l north_carolina\lang --grf north_carolina_town_names.grf north_carolina\north_carolina_town_names.nml
 %~dp0\nmlc\nmlc -l pennsylvania\lang --grf pennsylvania_town_names.grf pennsylvania\pennsylvania_town_names.nml
+%~dp0\nmlc\nmlc -l cuba\lang --grf cuba_town_names.grf cuba\cuba_town_names.nml
 
 ECHO Batch: Cleaning up
 RMDIR %~dp0\.nmlcache
@@ -15,10 +16,11 @@ if /I "%c%" EQU "TT" (goto :copy
 
 :copy
 ECHO Copying to NewGRF directory
-copy /y /v %~dp0\massachusetts_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
-copy /y /v %~dp0\colorado_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
-copy /y /v %~dp0\north_carolina_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
-copy /y /v %~dp0\pennsylvania_town_names.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\massachusetts_town_names.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\colorado_town_names.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\north_carolina_town_names.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\pennsylvania_town_names.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\cuba_town_names.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
 
 pause
 exit
